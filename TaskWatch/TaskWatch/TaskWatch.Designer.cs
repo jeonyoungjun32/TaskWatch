69d7ace7461ad41a3b9f9aa1c77824ecea1dc653
jeonyoungjun32 <87977528+jeonyoungjun32@users.noreply.github.com>
2025-11-05 14:16:53 +0900

    Task<T> 시간

---
+++ TaskWatch/TaskWatch/TaskWatch.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace ThredadWatch
+{
+    partial class TaskWatch
+    {
+        /// <summary>필수 디자이너 변수입니다.</summary>
+        private System.ComponentModel.IContainer components = null;
+        private Label dashBord;
+
+        /// <summary>사용 중인 모든 리소스를 정리합니다.</summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+        private void InitializeComponent()
+        {
+            this.dashBord = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dashBord
+            // 
+            this.dashBord.AutoSize = true;
+            this.dashBord.Font = new System.Drawing.Font("Consolas", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashBord.Location = new System.Drawing.Point(24, 24);
+            this.dashBord.Name = "dashBord";
+            this.dashBord.Size = new System.Drawing.Size(492, 55);
+            this.dashBord.TabIndex = 0;
+            this.dashBord.Text = "HH : mm : ss : fff";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Start);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "정지";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Stop);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 160);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dashBord);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thread Clock";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ TaskWatch/TaskWatch/TaskWatch.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThredadWatch
+{
+    public partial class TaskWatch : Form
+    {
+        private Thread clockThread; // 스레드
+        private volatile bool systemState = false; // volatile 뜻 즉 언제든지 값이 변경될수 있으니 항상 메모리에서 대기해라! 라고 컴파일러에게 알려준다.
+
+        public TaskWatch()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start(sender, e); // 시작
+        }
+
+        private void UpdateClock()
+        {
+            while (systemState)
+            {
+                try
+                {
+                    string nowTime = DateTime.Now.ToString("HH : mm : ss : fff");
+
+                    if (dashBord.IsHandleCreated)
+                    {
+                        dashBord.BeginInvoke((MethodInvoker)(() => dashBord.Text = nowTime));
+                    }
+
+                    Thread.Sleep(16);
+
+                }
+                catch (Exception ex)                {
+                    MessageBox.Show(ex.ToString());
+                    break;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            systemState = false;
+
+            if (clockThread != null && clockThread.IsAlive)
+            {
+                clockThread.Join(200);
+            }
+        }
+
+        /// <summary>
+        /// 시작
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Start(object sender, EventArgs e)
+        {
+            if (systemState)
+            {
+                this.button1.Enabled = false;
+                this.button2.Enabled = true;
+                return;
+            }
+
+            systemState = true;
+            clockThread = new Thread(UpdateClock)
+            {
+                IsBackground = true,
+                Name = "ClockThread"
+            };
+            clockThread.Start();
+
+            this.button1.Enabled = false;
+            this.button2.Enabled = true;
+        }
+
+        /// <summary>
+        /// 정지
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Stop(object sender, EventArgs e)
+        {
+            systemState = false;
+
+            if (clockThread != null && clockThread.IsAlive)
+            {
+                clockThread.Join(200);
+                clockThread = null;
+            }
+            Console.WriteLine(dashBord.Text);
+
+
+            this.button1.Enabled = true;
+            this.button2.Enabled = false;
+
+
+        }
+    }
+}
